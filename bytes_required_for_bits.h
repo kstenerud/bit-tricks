917eee6988297d4a1bb4c7d5ca152396753eb362
Karl Stenerud <kstenerud@gmail.com>
2019-09-12 08:21:07 +0200

    Update shifts

--- bytes_required_for_bits.h
+++ bytes_required_for_bits.h
@@ -1,18 +1,11 @@
 #pragma once
 
-#include "shifts.h"
+#include "comparisons.h"
 
 // Gives the number of bytes required to store x bits.
 //
-// The trick is in calculating the remainder of (bits / 8), and then turning
-// that into an addition of 0 or 1 to the byte count without branching.
-//
 static inline int get_bytes_required_for_bits(const int bits)
 {
-    // It's important that remainder is signed
-    const char remainder = bits & 7;
-    // ORing remainder with -remainder sets the high bit for any nonzero value.
-    // Logical shift right by 7, and you have 0 for 0, 1 for anything else.
-    const int extra_byte = asr_max(char, remainder | (-remainder)) & 1;
+    const int extra_byte = is_nonzero(bits & 7) & 1;
     return (bits >> 3) + extra_byte;
 }
--- shifts.h
+++ shifts.h
@@ -2,37 +2,42 @@
 
 #include <limits.h>
 
-// Get the size of X in bits - 1
+// Get the maximum amount that a value or type can be shifted.
+// This is the size of X in bits - 1, so for example, 7 for int8,
+// 15 for int16, 31 for int32, etc.
 //
-#define BITS_MAX(X) ( (sizeof(X) * CHAR_BIT - 1) )
+#define MAX_SHIFT_COUNT(X) ( (sizeof(X) * CHAR_BIT - 1) )
 
 // Logical shift left X by BITS
 //
 #define lsl(X, BITS) ( (X) << (BITS) )
 
-// Logical shift right X by BITS, using the specified SIGNED_TYPE
+// Logical shift right X by BITS, using the specified **unsigned** version of
+// SIGNED_TYPE. It needs to cast to the unsigned type to make sure the compiler
+// outputs LSR and not ASR.
 //
-#define lsr(SIGNED_TYPE, X, BITS) ( (unsigned SIGNED_TYPE)(X) >> (BITS) )
+#define lsr(SIGNED_TYPE, X, BITS) ( ((unsigned SIGNED_TYPE)(X)) >> (BITS) )
 
-// Arithmetic shift right X by BITS, using the specified SIGNED_TYPE
+// Arithmetic shift right X by BITS, using the specified SIGNED_TYPE.
+// This is technically implementation defined, but all major compilers output
+// an ASR for this code.
 //
-#define asr(SIGNED_TYPE, X, BITS) ( -(int)lsr(SIGNED_TYPE, X, BITS) )
+#define asr(SIGNED_TYPE, X, BITS) ( ((SIGNED_TYPE)(X)) >> (BITS) )
+
+// "Portable" version of asr. This causes most compilers to output extra instructions.
+// Usually, it outputs an LSR and NEG, whereas a signed shift would only output an ASR.
+//
+#define asr_portable(SIGNED_TYPE, X, BITS) ( -(SIGNED_TYPE)lsr(SIGNED_TYPE, X, BITS) )
 
 
 // Logical shift left by (size - 1)
 //
-#define lsl_max(X) lsl(X, BITS_MAX(X))
+#define lsl_max(X) lsl(X, MAX_SHIFT_COUNT(X))
 
 // Logical shift right by size - 1
 //
-#define lsr_max(SIGNED_TYPE, X) lsr(SIGNED_TYPE, X, BITS_MAX(SIGNED_TYPE))
+#define lsr_max(SIGNED_TYPE, X) lsr(SIGNED_TYPE, X, MAX_SHIFT_COUNT(SIGNED_TYPE))
 
 // Arithmetic shift right by size - 1
 //
-#define asr_max(SIGNED_TYPE, X) ( -(int)lsr_max(SIGNED_TYPE, X) )
-
-
-// Non-portable arithmetic shift right by size - 1
-// Right shifts of signed integers is implementation-defined
-//
-#define asr_max_np(X) ((X) >> BITS_MAX(X))
+#define asr_max(SIGNED_TYPE, X) asr(SIGNED_TYPE, X, MAX_SHIFT_COUNT(SIGNED_TYPE))
